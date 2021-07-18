a3096392a04ac62264ac2c2797e88961a36e27ea
Jahaslito <jahaslito24@gmail.com>
2021-07-18 16:41:25 +0300

    Custom register view

--- FITA/resources/views/auth/index.blade.php
+++ FITA/resources/views/auth/index.blade.php
@@ -1,38 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-<!--	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>-->
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate/animate.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/css-hamburgers/hamburgers.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animsition/css/animsition.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/select2/select2.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/daterangepicker/daterangepicker.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
-<!--===============================================================================================-->
-</head>
-<body>
-
+@extends('layouts.custom')
+@section('content')
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -42,14 +9,14 @@
 					</span>
 				</div>
 
-				<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+				<form class="login100-form " method="POST" action="{{ route('login') }}">
                     @csrf
 
-                    <div class="wrap-input100 validate-input m-b-26" >
+                    <div class="wrap-input100 form-group m-b-26" >
                         <label for="email" class="label-input100">{{ __('Username') }}</label>
 
                         <div class="col-md-6">
-                            <input id="email" type="email" class=" input100 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" placeholder="Enter your username" class=" input100 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -59,11 +26,11 @@
                         </div>
                     </div>
 
-                    <div class="wrap-input100 validate-input m-b-18">
+                    <div class="wrap-input100 form-group m-b-18">
                         <label for="password" class="label-input100">{{ __('Password') }}</label>
 
                         <div class="col-md-6">
-                            <input id="password" type="password" class=" input100 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <input id="password" type="password" placeholder="Enter your password" class=" input100 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                             @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -106,22 +73,7 @@
 		</div>
 	</div>
 
-<!--===============================================================================================-->
-	<script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('vendor/animsition/js/animsition.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('vendor/bootstrap/js/popper.js') }}"></script>
-	<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('vendor/select2/select2.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('vendor/daterangepicker/moment.min.js') }}"></script>
-	<script src="{{ asset('vendor/daterangepicker/daterangepicker.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('vendor/countdowntime/countdowntime.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('js/main.js')}}"></script>
 
 </body>
 </html>
+@endsection
--- FITA/resources/views/auth/register.blade.php
+++ FITA/resources/views/auth/register.blade.php
@@ -1,21 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.custom')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <body style="overflow: hidden">
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="login100-form-title" style="background-image: url(/public/images/background.png);">
+					<span class="login100-form-title-1">
+						{{ __('Register') }}
+					</span>
+                </div>
 
-                <div class="card-body">
+                <div class="login100-form ">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="wrap-input100  m-b-26">
+                            <label for="name" class="label-input100">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" placeholder="Enter your name" class="input100 form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -25,11 +30,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="wrap-input100 validate-input m-b-26">
+                            <label for="email" class="label-input100 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" placeholder="Enter your email address" class="input100 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -39,11 +44,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="wrap-input100 validate-input m-b-26">
+                            <label for="password" class="label-input100">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" placeholder="Enter your password" class=" input100 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -53,20 +58,25 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <div class="wrap-input100 validate-input m-b-26">
+                            <label for="password-confirm" class="label-input100">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" placeholder="Confirm your password" class=" input100 form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                        <div class="flex-sb-m w-full p-b-30">
+                            <div class=" mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="login100-form-btn">
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
                             </div>
+                            <a class="btn btn-link " href="{{ route('login') }}" style="margin-right: 60px">
+                                {{ __('Login') }}
+                            </a>
                         </div>
                     </form>
                 </div>
@@ -74,4 +84,5 @@
         </div>
     </div>
 </div>
+    </body>
 @endsection
