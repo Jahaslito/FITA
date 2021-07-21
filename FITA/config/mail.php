a16097eea7392fd10357f97f8fe98c22f24df50f
Jahaslito <jahaslito24@gmail.com>
2021-07-21 18:31:40 +0300

    Email verification done

--- FITA/config/mail.php
+++ FITA/config/mail.php
@@ -83,13 +83,13 @@ return [
     |
     */
 
-    'stream' => [
-        'ssl' =>[
-            'allow_self_signed'=>true,
-            'verify_peer'=>false,
-            'verify_peer_name'=>false,
-        ],
-    ],
+//    'stream' => [
+//        'ssl' =>[
+//            'allow_self_signed'=>true,
+//            'verify_peer'=>false,
+//            'verify_peer_name'=>false,
+//        ],
+//    ],
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'jahaslito24@gmail.com'),
